805000c028823649f9d5895ba6ce65ca4c29bbe9
Hidayatus Sholikhin <71644759+hidiyitis@users.noreply.github.com>
2025-07-25 17:50:44 +0700

    Update cors-middleware.js

--- src/middlewares/cors-middleware.js
+++ src/middlewares/cors-middleware.js
@@ -1,20 +1,8 @@
 import cors from 'cors';
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://sixti-fe-836115399739.asia-southeast2.run.app',
-];
-
 const corsMiddleware = cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
+  origin: true,  // Reflects the requesting origin
+  credentials: true, // Allow cookies/auth headers if needed
 });
 
 export default corsMiddleware;
